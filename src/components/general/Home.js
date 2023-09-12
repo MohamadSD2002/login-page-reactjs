f4a19bdc8b8dc74a23a71b5aabd5769b388090e2
MohamadSedighBakhshaish <118728086+MohamadSedighBakhshaish@users.noreply.github.com>
2023-09-12 14:45:13 +0330

    Update Home.js

--- src/components/general/Home.js
+++ src/components/general/Home.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "../../assets/css/homeStyle.css";
-// import { Link } from "react-router-dom";
 import Logout from "../utills/logout";
 
 export default function Home({ setIsAuth }) {
   return (
     <div className="home">
       <h1>Welcome To Home</h1>
-      {/* <Link className="btn" to="/login">
-        Login
-      </Link> */}
+    
       <Logout setIsAuth={setIsAuth} />
     </div>
   );
