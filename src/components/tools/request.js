ad850517888802cda7c90e4c85768eb9ea1b49d0
MohamadSedighBakhshaish <118728086+MohamadSedighBakhshaish@users.noreply.github.com>
2023-09-12 15:24:15 +0330

    Update request.js

--- src/components/tools/request.js
+++ src/components/tools/request.js
@@ -3,6 +3,26 @@ import { BASE_URL } from "./constants";
 
 const request = axios.create({
   baseURL: BASE_URL,
+  timeout: 2000,
+  headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
+
+request.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+request.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default request;
